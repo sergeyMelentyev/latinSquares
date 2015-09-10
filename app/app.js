3a3d590e0b015a0fe327b7225fcaa6fa0d3a6c84
Sergey Melentyev <sergey.melentyev@gmail.com>
2015-09-10 17:54:36 +0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,24 @@
-test
+'use strict';
+
+var latinSquaresGame = angular.module('latinSquaresGame',['ui.router', 'gridster']);
+
+latinSquaresGame.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/age");
+	$stateProvider
+		.state('age', {
+			url: "/age",
+				views: {
+					"viewA": {
+						templateUrl: "partials/age.html"
+					}
+				}
+		})
+		.state('theGame', {
+			url: "/theGame",
+				views: {
+					"viewA": {
+						templateUrl: "partials/theGame.html"
+					}
+				}
+		})
+});
